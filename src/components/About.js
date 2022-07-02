74abcab28e13d336cb95b74e18cb1fba769bc0f7
Bro <bro611897@gmail.com>
2022-07-02 12:36:07 +0530

    Updated about js

--- src/components/About.js
+++ src/components/About.js
@@ -5,12 +5,36 @@ export default function About(props) {
         <div className="container my-3" >
             <div className="container containerAbout" style={{backgroundColor: props.mode === 'light' ? 'dark': 'light', color: props.mode === 'dark' ? 'light': 'dark', border: props.mode === 'dark' ? 'light':'dark'}}>
                 <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi veniam debitis laborum enim minima ex sed tempora rem. Quas beatae ullam sit in. Illum similique, sit sapiente velit animi architecto repellat autem in, reprehenderit labore aperiam optio, veritatis laboriosam a tempore at perspiciatis voluptas! Eveniet quas incidunt rem beatae minima!
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ex ipsum officia repellat, explicabo harum assumenda possimus in vitae unde, odit tempore cum optio repellendus. Omnis, molestiae et aut sapiente tempora, repellendus voluptatibus totam esse earum aperiam dolorem porro voluptate maiores nisi. Quam obcaecati temporibus neque animi molestias ut eos necessitatibus debitis tenetur, aliquam magnam cum deleniti optio, numquam assumenda aspernatur rem inventore, distinctio vitae et exercitationem. Doloribus fugit atque ratione eos magnam in ipsum sequi. Voluptatum dolore, corrupti mollitia sed quibusdam officiis autem quas inventore assumenda est itaque dicta, nobis dolorem temporibus odio aspernatur. Aspernatur magnam tempora eos? Consequatur, blanditiis.
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore dignissimos rerum odit modi rem non mollitia cumque dicta illo fuga.
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere, at nobis asperiores qui voluptatum eos. Voluptates odio doloremque voluptas explicabo officiis, porro aliquid? Quod odio eveniet suscipit a officia eius repudiandae ad, amet exercitationem ducimus velit atque saepe, animi quidem. Eligendi quo omnis reiciendis facilis, exercitationem et soluta maiores! Ex numquam ipsam molestiae repellendus, officia hic quisquam quis? Pariatur, nisi! Obcaecati est veritatis optio delectus deleniti odit libero similique maiores sequi molestiae. Earum quae consequuntur asperiores, quod pariatur ducimus dignissimos quasi cupiditate? Omnis sed voluptates saepe quaerat perferendis, reprehenderit ratione perspiciatis soluta iure illo ipsum cum doloremque ullam non sunt!
-                    
+                BE A TEXT NINJA USE TEXT NINJA.
+                    Text Ninja is a simple to use text analytics web based application, why "web", because you don't need to install it in you phone,
+                    And you don't have to give extra memory for this application.
+
+                    It is a lightweight web application, for analyzing your text, It has all necessary features,
+                    <br/>
+                    Like:
+                    <br/>
                 </p>
+                <p>1. Text Capitalization after full stops.</p>
+                    
+                    <p>2. Uppercasing each letter of your phrase.</p>
+                    
+                    <p>3. Lowercasing each letter of your phrase.</p>
+                    
+                    <p>4. Removing unwanted spaces from your phrase.</p>
+                    
+                    <p>5. Copying a huge text file without lagging.</p>
+                    
+                    <p>6. Letter counter for your phrase.</p>
+                    
+                    <p>7. Words counter for your phrase.</p>
+                    
+                    <p>8. A Timer, about how much time a normal reader may take to read your phrase.</p>
+                    
+                    <p>9. Text Preview for you text.</p>
+                    
+                    <p>10. With Dark mode, so you can even use it at night.</p>
+                    
+                    <p>11. 3 Major color themes for both dark mode and light mode.</p>
             </div>
         </div>
     )
